d165b480b021c663ae30946aaccb6ed25f249071
Jimmy-Pingcas <Jimmypingcas415@gmail.com>
2026-05-23 19:10:28 +0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`\n\nServer running at http://localhost:${PORT}\n\n\n`);
+});
